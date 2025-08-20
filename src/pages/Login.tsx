29537c8fa03bd732db2095c3edb871d84592facd
Bilal Alsayad <bilal.alsayad5@gmail.com>
2025-08-20 15:19:57 +0300

    22

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -37,8 +37,6 @@ export default function Login() {
     );
     const data = await res.json();
 
-    console.log(data);
-
     if (!data.success) {
       alert("Email or Password is wrong");
     }
